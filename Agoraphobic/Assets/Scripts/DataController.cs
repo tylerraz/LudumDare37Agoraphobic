363c2f5cdc30d05d640b0352f19593d610f7680c
Tyler Rasmussen <tylerraz@gmail.com>
2016-12-09 23:16:16 -0500

    GUI_Skeleton
    MainMenu Gui layout prototype

---
+++ Agoraphobic/Assets/Scripts/DataController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+
+
+
+public class DataController : MonoBehaviour {
+
+	
+    
+    // Use this for initialization
+	void Start () {
+
+        DontDestroyOnLoad(gameObject);
+        SceneManager.LoadScene("MainMenu");
+
+	}
+	
+
+
+
+
+}
