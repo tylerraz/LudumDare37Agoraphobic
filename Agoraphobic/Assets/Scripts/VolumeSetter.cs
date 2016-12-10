8e74811e20eda33addbd930c861674b5b3ff4825
Tyler Rasmussen <tylerraz@gmail.com>
2016-12-10 01:10:08 -0500

    GenderChoosePanel
    Added gender choose panel, placeholder art only

---
+++ Agoraphobic/Assets/Scripts/VolumeSetter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class VolumeSetter : MonoBehaviour {
+
+    public GameObject settingPanel;
+    public AudioSource audioSource;
+
+
+
+	// Use this for initialization
+	void Start () {
+        settingPanel.SetActive(false);
+
+    }
+
+
+    public void ActivateSettings() {
+
+        settingPanel.SetActive(true);
+
+    }
+
+   public void CloseSettings() {
+
+        settingPanel.SetActive(false);
+
+    }
+
+
+
+
+
+
+
+
+}
